eec35e53aab98af9eb60f28e374d4679acb6696c
caribai <caribai93@gmail.com>
2022-01-22 16:50:19 -0300

    ensayo

--- consultas.js
+++ consultas.js
@@ -12,8 +12,16 @@ async function getcustomers() {
     return result.rows
 }
 async function getStores() {
-    const result = await pool.query(`SELECT store_name FROM stores`);
+    const result = await pool.query(`SELECT store_name FROM stores ORDER BY store_name`);
+    return result.rows
+}
+async function getCategories() {
+    const result = await pool.query(`SELECT category_name FROM categories ORDER BY category_name`);
+    return result.rows
+}
+async function getBrands() {
+    const result = await pool.query(`SELECT brand_name FROM brands ORDER BY brand_name`);
     return result.rows
 }
 
-module.exports = {getcustomers,getStores}
+module.exports = {getcustomers,getStores,getCategories,getBrands}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const {getStores} = require("./consultas.js")
+const {getStores,getCategories,getBrands} = require("./consultas.js")
 
 app.listen(3000, ()=>{
     console.log("Escuchando el puerto 3000")
@@ -9,7 +9,16 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get("/usuarios" && req.method == "GET") => {
+app.get("/stores", async function (req, res) {
     const stores = await getStores();
     res.end(JSON.stringify(stores))
-}
+});
+
+app.get("/categories", async function (req, res) {
+    const categories = await getCategories();
+    res.end(JSON.stringify(categories))
+});
+app.get("/brands", async function (req, res) {
+    const brands = await getBrands();
+    res.end(JSON.stringify(brands))
+});
