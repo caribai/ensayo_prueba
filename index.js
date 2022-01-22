f1798dca6f18eb4d924542079e55757efd74de2b
caribai <caribai93@gmail.com>
2022-01-22 15:55:50 -0300

    ensayo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const {getcustomers} = require("./consultas.js")
+const {getStores} = require("./consultas.js")
 
 app.listen(3000, ()=>{
     console.log("Escuchando el puerto 3000")
@@ -8,3 +8,8 @@ app.listen(3000, ()=>{
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get("/usuarios" && req.method == "GET") => {
+    const stores = await getStores();
+    res.end(JSON.stringify(stores))
+}
